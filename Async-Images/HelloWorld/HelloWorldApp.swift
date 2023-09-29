212f5558371b56f9d9b83db6fecadd8f98d4314d
prannvat <53794605+prannvat@users.noreply.github.com>
2023-09-29 16:36:04 +0100

    Async Images using URLS

---
+++ Async-Images/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Prannvat Singh on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
